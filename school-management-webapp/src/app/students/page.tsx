a1f9904cf23150c5a154a71ec7fa9a2645277142
qcuong711 <dangquangcuong711@gmail.com>
2025-07-08 10:05:15 +0700

    xoa sch

--- school-management-webapp/src/app/students/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function StudentsPage() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Manage Students</h1>
-      <p>This is where the student management interface will be.</p>
-    </div>
-  );
-} 
